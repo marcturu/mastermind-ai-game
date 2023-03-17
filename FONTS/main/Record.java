4b6b4c7b8fe9eccc4d680e02c34609cb010be67a
ferran.solanes <ferran.solanes@estudiantat.upc.edu>
2023-03-17 10:22:18 +0100

    Creació de les classes. Sense subclasses ni enumerations. També està el cos de Record.java

---
+++ FONTS/main/Record.java
@@ -0,0 +1,23 @@
+
+
+public static class Record() {
+    String nom_record;
+    String nom_usuari;
+    int punts;
+
+    public String get_nom_record() {
+
+    }
+
+    public String getNom_usuari() {
+
+    }
+
+    public int getPunts() {
+
+    }
+
+    public boolean check_if_record(int punts, String nom_usuari) {
+
+    }
+}main.domain.classes;
