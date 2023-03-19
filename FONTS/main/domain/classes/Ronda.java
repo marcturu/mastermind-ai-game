ab2f3065a700e72abdc88ebaad3435eade42bdb7
jordi.baranda <jordi.baranda@estudiantat.upc.edu>
2023-03-19 15:07:35 +0100

    Ronda

--- FONTS/main/domain/classes/Ronda.java
+++ FONTS/main/domain/classes/Ronda.java
@@ -15,6 +15,31 @@ public static class Ronda  {
 
     }
 
-    public Se
+    public Sequencia getSequencia_intentada() {
+        return sequencia_intentada;
+    }
 
+    public Sequencia getSequencia_verificacio() {
+        return sequencia_verificacio;
+    }
+
+    public int getId_ronda() {
+        return id_ronda;
+    }
+
+    public int getId_partida() {
+        return id_partida;
+    }
+
+    public int getNum_ronda() {
+        return num_ronda;
+    }
+
+    public void setSequencia_intentada(Sequencia sequencia_intentada) {
+        this.sequencia_intentada = sequencia_intentada;
+    }
+
+    public void setSequencia_verificacio(Sequencia sequencia_verificacio) {
+        this.sequencia_verificacio = sequencia_verificacio;
+    }
 }
