668577c9fe38e53cdf35472956ea124ce5c2ee55
jordi.baranda <jordi.baranda@estudiantat.upc.edu>
2023-03-19 14:26:22 +0100

    Sequencia

---
+++ FONTS/main/Sequencia.java
@@ -0,0 +1,39 @@
+package main.domain.classes;
+
+import java.util.*;
+
+public class Sequencia {
+    private enum Colors{
+        Vermell;
+        Verd;
+        Blau;
+        groc;
+        magenta;
+        cian;
+        blanc;
+        negre;
+        marro;
+        gris;
+    }
+
+    public enum Type_seq{
+        intentada;
+        verificacio;
+    }
+
+    Vector array_colors(4);
+    Type_seq tipus;
+
+    public Sequencia(Vector color, Type_seq tipo){
+
+    }
+
+    public get_array(){
+
+    }
+
+    public valida_sequencia(){
+
+    }
+
+}
