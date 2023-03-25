c060d84bbe77a3ad12011dc503e358ddcc9cba2b
matux <marc.turu@estudiantat.upc.edu>
2023-03-25 19:30:43 +0100

    Classe User 2

--- FONTS/main/domain/classes/User.java
+++ FONTS/main/domain/classes/User.java
@@ -3,4 +3,30 @@ package main.domain.classes;
 import java.util.*;
 
 public class User {
+    private int id;
+    private String nom;
+    private Type_user tipus_user;
+
+    public User(int id, String nom, Type_user tipus_user) {
+        this.id = id;
+        this.nom = nom;
+        this.tipus_user = tipus_user;
+    }
+
+    public int get_id() {
+        return id;
+    }
+
+    public String get_nom() {
+        return nom;
+    }
+
+    public void set_nom(String nom) {
+        this.nom = nom;
+    }
+
+    public Type_user get_tipus_user() {
+        return tipus_user;
+    }
+
 }
