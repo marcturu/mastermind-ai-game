04fdab319846abfa815eee1f072c90e7ef6da957
jordi.baranda <jordi.baranda@estudiantat.upc.edu>
2023-03-28 19:51:45 +0200

    error package enumerations

--- FONTS/main/domain/classes/enumerations/colors.java
+++ FONTS/main/domain/classes/enumerations/colors.java
@@ -1,4 +1,4 @@
-package main.domain.classes;
+package main.domain.classes.enumerations;
 
 public enum colors {
     VERMELL("vermell",1),      //Vermell
--- FONTS/main/domain/classes/enumerations/type_seq.java
+++ FONTS/main/domain/classes/enumerations/type_seq.java
@@ -1,8 +1,8 @@
-package main.domain.classes;
+package main.domain.classes.enumerations;
 
 public enum type_seq {
     solucio,
     intentada,
-    verificacio;
+    verificacio
 
 }
