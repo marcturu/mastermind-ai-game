84f4f023fba5d5e93a269ba95cfcfffd6365294d
jordi.baranda <jordi.baranda@estudiantat.upc.edu>
2023-03-29 13:45:52 +0200

    Classe Ronda acabada

--- FONTS/main/domain/classes/Ronda.java
+++ FONTS/main/domain/classes/Ronda.java
@@ -55,5 +55,13 @@ public class Ronda  {
         }
     }
 
-    //mirar si ha encertat el intent la solucio
+    public boolean check_sequencia_encertada(){
+        colors[] array = sequencia_verificacio.get_array();
+        int n = 0;
+        for (int i = 0; i < array.length; ++i ){
+            if (array.get_id_color == 10) ++n;
+        }
+        return n == 4;
+    }
+
 }
