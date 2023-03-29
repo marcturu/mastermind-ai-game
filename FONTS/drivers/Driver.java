798c7db0551893c61f4448e6f572a84ac0250295
jordi.baranda <jordi.baranda@estudiantat.upc.edu>
2023-03-29 22:57:47 +0200

    driver

--- FONTS/drivers/Driver.java
+++ FONTS/drivers/Driver.java
@@ -9,7 +9,7 @@ import java.util.*;
 public class Driver {
 
     public static void main(String[] args) {
-        System.out.println("Mastermind (PROP grup 13.2)");
+        System.out.println("Mastermind (PROP grup 13.2) ");
         Scanner s = new Scanner(System.in);
         String input = s.nextLine();
         System.out.println(input);
