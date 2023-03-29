1bcd0448667fe290b7dc961750566362ea71e974
jordi.baranda <jordi.baranda@estudiantat.upc.edu>
2023-03-29 22:55:47 +0200

    driver

---
+++ FONTS/drivers/Driver.java
@@ -0,0 +1,17 @@
+package drivers;
+
+/*import main.domain.classes.*;
+import main.domain.classes.enumerations.*;
+import main.domain.controller.Controlador_Domini;*/
+
+import java.util.*;
+
+public class Driver {
+
+    public static void main(String[] args) {
+        System.out.println("Mastermind (PROP grup 13.2)");
+        Scanner s = new Scanner(System.in);
+        String input = s.nextLine();
+        System.out.println(input);
+    }
+}
