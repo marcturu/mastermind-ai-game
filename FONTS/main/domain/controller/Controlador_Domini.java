6687ed93d769450fed4a58e8b8572bfe2aa533f5
matux <marc.turu@estudiantat.upc.edu>
2023-03-31 21:05:56 +0200

    Classes User, User_persona, User_maquina i respectius tests + Controlador_Domini (no acabat)

--- FONTS/main/domain/controller/Controlador_Domini.java
+++ FONTS/main/domain/controller/Controlador_Domini.java
@@ -19,17 +19,18 @@ import java.util.*;
 
 public class Controlador_Domini {
     private User Usuari;
-    private User_persona Usuari_perosna;
-    private User_maquina Usuari_maquina;
     private Record Record;
     private Ranking Ranking;
     private Controlador_Partida CtrlPartida;
-    private HashMap<string,User> hashUsers;
+    private HashMap<String,User> hashUsers;
+    private static Controlador_Domini singletonObject;
 
     public Controlador_Domini() {
         this.usuari = null;
         this.CtrlPartida = new Controlador_Partida();
         this.hashUsers = new hashUsers<>();
+        this.CtrlPartida =
+        this.hashUsers = new HashMap<String,User>();
 
     }
 
@@ -44,23 +45,27 @@ public class Controlador_Domini {
      * Crea una partida nova amb els parametres entrats. Aquests parametres han d'estar conprovats abans de cridar a la funció.
      */
 
-    // CREADORA SI ÉS USER_PERSONA
-    public void creacioUsuari(int id, String nom, Type_user tipus_user, String password, int num_rondes_totals, int num_partides_totals, int puntuacio, int partides_guanyades, List<Partida> llista_partides_no_acabades, List<Partida> llista_partides_acabades) {
-        this.Usuari_persona = new User_persona(id, string, tipus_user, password, num_rondes_totals, num_partides_totals, puntuacio, partides_guanyades, llista_partides_no_acabades, llista_partides_acabades);
-        //hashUsers.put(nom, Usuari);
+    public Controlador_Domini(){
+        this.Usuari = null;
+        this.Record = null;
+        this.Ranking = null;
+
     }
 
-    // CREADORA SI ÉS USER_MAQUINA
-    public void creacioUsuari(int id, String nom, Type_user tipus_user, boolean genetic_algorithm, int num_rondes_totals, int num_partides_totals, int puntuacio, int partides_guanyades, List<Partida> llista_partides_no_acabades, List<Partida> llista_partides_acabades) {
-        this.Usuari_maquina = new User_maquina(id, string, tipus_user, genetic_algorithm, num_rondes_totals, num_partides_totals, puntuacio, partides_guanyades, llista_partides_no_acabades, llista_partides_acabades);
-        //hashUsers.put(nom, Usuari);
+    //Pre: Es rep un nom d'usuari d'usuari i un tipus d'usuari (de moment no es té en compte ni la contrassenya, si és persona, ni el tipus d'alogorisme, si és màquina)
+    //Post: Es crea el usuari amb el nom i tipus rebut com a parametre i s'afageix al map.
+    public void iniciaInstanciaUser(String nom, Type_user tipus_user) { //quan es treballi amb log in es passarà també la contrasenya.
+        List<Partida> llista_partides_no_acabades;
+        List<Partida> llista_partides_acabades;
+        if (string.equals("User_persona")) Usuari = new User_persona(hashUsers.size()+1, nom, tipus_user, 0, 0, 0, 0, llista_partides_no_acabades, llista_partides_acabades);
+        else Usuari = new User_maquina(hashUsers.size()+1, nom, tipus_user, 0, 0, 0, 0, llista_partides_no_acabades, llista_partides_acabades);
+        hashUsers.put(string, Usuari);
     }
 
-    public void creacioUsuari(int id, String nom, Type_user tipus_user) {
-        this.Usuari = new User(id, string, tipus_user);
-        hashUsers.put(nom, Usuari);
+    public static Controlador_Domini get_CtrlDomini {
+        if (singletonObject == null) singletonObject = new Controlador_Domini();
+        return singletonObject;
     }
-
     public void creacioRecord(String nom_record) {
         this.Record = new Record(nom_record);
     }
@@ -68,6 +73,12 @@ public class Controlador_Domini {
     public boolean es_record(int punts, String nom_usuari) {
         this.Record.check_if_record(punts, nom_usuari);
     }
+
+    public void elimina_usuari(User usuari) {
+        hashUsers.remove(usuari.get_nom(), usuari);
+    }
+
+
 
 
 
