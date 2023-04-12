edc2a32c19f2f7d21ff68495e12af5f7a7d607ef
matux <marc.turu@estudiantat.upc.edu>
2023-04-12 12:10:40 +0200

    Canvis Exceptions

--- FONTS/main/domain/classes/User.java
+++ FONTS/main/domain/classes/User.java
@@ -5,8 +5,7 @@ import java.util.List;
 import java.util.Vector;
 
 //import main.domain.classes.Ranking;
-import main.domain.classes.enumerations.Type_user;
-import main.domain.classes.exceptions.MyException;
+import main.domain.classes.enumerations.*;
 
 
 public class User {
@@ -137,8 +136,7 @@ public class User {
         return null;
     }
 
-    //Ja es comprova que la llista no està plena (<10)
-    public void afegir_partida_nova(Partida partida_nova) throws Exception {
+    public void afegir_partida_nova(Partida partida_nova){
         llista_partides_no_acabades.add(partida_nova);
         incrementar_partides_totals();
     }
@@ -161,11 +159,11 @@ public class User {
         llista_partides_no_acabades.remove(partida_acabada);
     }
 
-    public Partida get_partida_acabada(int id_partida) throws MyException{
+    public Partida get_partida_acabada(int id_partida) throws Exception{
         for (int i = 0; i < get_num_partides_acabades(); i++) {
             if ((llista_partides_acabades.get(i)).get_id() == id_partida) return llista_partides_acabades.get(i);
         }
-        throw new MyException("No esta la partida");
+        throw new Exception("No esta la partida");
     }
 
     public double get_puntuacioPvsP() throws Exception{
