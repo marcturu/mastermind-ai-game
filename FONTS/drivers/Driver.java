bd17cb26f73fb9689aecb1f004490e02708b385e
jordi.baranda <jordi.baranda@estudiantat.upc.edu>
2023-04-14 13:29:47 +0200

    varios

--- FONTS/drivers/Driver.java
+++ FONTS/drivers/Driver.java
@@ -218,7 +218,7 @@ public class Driver {
             codemaker_entra_solucio(dif);
         }
 
-        while (ronda_actual <= dif.get_num_max_rondes() && !domini.partida_acabada() ) {
+        while (ronda_actual <= dif.get_num_max_rondes() && !domini.partida_acabada() && !acabar) {
             System.out.println("Dessitges guardar la partida a mitges? \n Introdueix: true or false");
             acabar = in.nextBoolean();
             if (!acabar){
@@ -235,22 +235,17 @@ public class Driver {
                         }
                     }
                 }
-
                 Sequencia seq_int = codebreaker_entra_intentada(dif);
-
                 Sequencia seq_ver = codemaker_entra_verificacio(seq_int.get_array());
-
-                System.out.println("Peta Aqui?\n" + dif.get_num_max_rondes() + ronda_actual);
                 try {
                     domini.jugar_ronda(seq_int, seq_ver);
                 }catch (Exception ex){
                     System.out.println(ex.getMessage());
                 }
-                System.out.println("bucldin?\n" + domini.partida_acabada());
                 if (ronda_actual == 10) domini.tractament_partida_acabada();
             }
             else {
-
+                domini.guardar_partida_a_mitges()
             }
 
         }
--- FONTS/main/domain/classes/User.java
+++ FONTS/main/domain/classes/User.java
@@ -173,6 +173,23 @@ public class User {
         return null;
     }
 
+    public List<Integer> get_ids_partides_no_acabades() {
+        ArrayList<Integer> llista = new ArrayList<Integer>();
+        for (int i = 0; i < llista_partides_no_acabades.size(); i++) {
+            llista.add((llista_partides_acabades.get(i)).get_id());
+        }
+        return llista;
+    }
+
+    public void actualitza_partida_actual(int id,Partida par) {
+        for (int i = 0; i < get_num_partides_actuals(); i++) {
+            if ((llista_partides_no_acabades.get(i)).get_id() == id) {
+                llista_partides_no_acabades.remove(i);
+                llista_partides_no_acabades.add(par);
+            }
+        }
+    }
+
     /*
     //USER_PERSONA
     public void set_password(String password) {}
