66832c5b2ad1b1799154ae8d34cf15a2f3aba423
jordibd44 <jordibd44@gmail.com>
2023-05-11 11:49:05 +0200

    ctrl user(mal)

--- FONTS/main/persistence/ctrl_user.java
+++ FONTS/main/persistence/ctrl_user.java
@@ -4,6 +4,10 @@ import com.google.gson.Gson;
 import java.io.FileWriter;
 import java.io.File;
 import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
 import java.util.HashMap;
 
 import main.domain.classes.User;
@@ -30,5 +34,28 @@ public class ctrl_user {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public HashMap<String,User> carrega_list_user(){
+        Gson gson = new Gson();
+        String archivo = "../EXE/dades/list_user.json";
+
+        String contenido = "";
+        try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
+            String linea;
+            while ((linea = br.readLine()) != null){
+                contenido += linea;
+            }
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
+
+        if (!contenido.isEmpty()){
+            Type type = new TypeToken<HashMap<String,User>>(){}.getType();
+            HashMap<String,User> userHashMap = gson.fromJson(contenido,type);
+            return userHashMap;
+        }
+        else return null;
+
     }
 }
