59dc4477abeff6c2efffd0198307fc18e4c632e0
matux <marc.turu@estudiantat.upc.edu>
2023-05-12 20:59:42 +0200

    CtrlS: Ranking, Record bases persistència

---
+++ FONTS/main/persistence/ctrl_ranking.java
@@ -0,0 +1,25 @@
+package main.persistence;
+
+import com.google.gson.Gson;
+import java.io.FileWriter;
+import java.io.File;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
+import java.util.HashMap;
+
+import main.domain.classes.Ranking;
+import main.domain.classes.enumerations.dificultats;
+
+public class ctrl_ranking {
+
+    public void save_ranking(Ranking ranking) {
+    }
+
+    public Ranking carrega_record(Dificultats dificultat) {
+    }
+
+}
+
---
+++ FONTS/main/persistence/ctrl_record.java
@@ -0,0 +1,26 @@
+package main.persistence;
+
+import com.google.gson.Gson;
+import java.io.FileWriter;
+import java.io.File;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
+import java.util.HashMap;
+
+import main.domain.classes.Record
+import main.domain.classes.RecordDouble;
+import main.domain.classes.RecordInteger;
+import main.domain.classes.RecordLong;
+
+public class ctrl_ranking {
+
+    public void save_record(Record record) {
+    }
+
+    public Record carrega_record(int nom_record) {
+    }
+
+}
