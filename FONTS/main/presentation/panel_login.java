6da01311899d84d55d8d668d2986601a64150f69
matux <marc.turu@estudiantat.upc.edu>
2023-06-01 22:36:01 +0200

    Canvis registres login

--- FONTS/main/presentation/panel_login.java
+++ FONTS/main/presentation/panel_login.java
@@ -1,9 +1,6 @@
 package main.presentation;
 
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextArea;
+import javax.swing.*;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
--- FONTS/main/presentation/panel_user.java
+++ FONTS/main/presentation/panel_user.java
@@ -1,7 +1,6 @@
 package main.presentation;
 
-import javax.swing.JButton;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 import main.presentation.controller.Controlador_Presentacio;
 
@@ -56,6 +55,8 @@ public class panel_user extends JPanel{
         b_logout.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 ctrlPresentacio.canvia_a_inici();
+                String logout_correcte = "Sessió tancada";
+                JOptionPane.showMessageDialog(null, logout_correcte, "", JOptionPane.PLAIN_MESSAGE);
             }
         });
 
